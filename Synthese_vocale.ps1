fcf0f89bc9f08cb79ff9badb042180f2297290f1
witkowski <fabrice.witkowski@gmail.com>
2020-06-17 01:56:19 +0200

    RTC
    RTC

--- Synthese_vocale.ps1
+++ Synthese_vocale.ps1
@@ -4,3 +4,7 @@ $speak.Speak("Entrez votre nom")
 $a = read-host "Entrez votre nom"
 $speak.Speak("Entrez votre mot de passe")
 $b = read-host "Entrez votre mot de passe"
+# on récupère la liste des membres de la methode.
+$test = $speak | Get-Member
+# et on fait un rapport vocal
+$speak.Speak($test)
