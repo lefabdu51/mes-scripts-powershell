949b1cf7fbe8223a8b287579fffa46e61a8a0304
witkowski <fabrice.witkowski@gmail.com>
2020-06-23 00:23:06 +0200

    RTC

--- installation_logiciels.ps1
+++ installation_logiciels.ps1
@@ -1,7 +1,8 @@
 ﻿$ListeProgrammes = Get-WmiObject -Class Win32_Product | Select-Object -Property Name
-$ListeProgrammesD = gp HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |Select DisplayName, DisplayVersion, Publisher, InstallDate, HelpLink, UninstallString
-function recup_fichier ($uri,$sortie)
-{
+# $ListeProgrammesD = gp HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |Select DisplayName, DisplayVersion, Publisher, InstallDate, HelpLink, UninstallString
+$ListeProgrammesD = gp HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |Select DisplayName
+function recup_fichier  {
+param ([string] $uri,[string] $sortie)
 #la fonction sert à récupérer les fichier de setup concernant la nouvelle / dernière version d'un logiciel.
 #la commande à utiliser est invoke-webrequest, qui est un équivalent à wget sous linux.
 # Recuperation d'un fichier setup
@@ -11,8 +12,10 @@ Invoke-WebRequest -Uri "$url" -OutFile "$sortie"
 # si 500  afficher message suivant : erreur serveur
 # si 450:  afficher message suivant : erreur de droits
 }
-function test_existence_logiciel($ListeProgrammes,$ListeProgrammesD,$nom)
-{
+$ListeProgrammesD 
+function test_existence_logiciel  {
+
+param ([object] $ListeProgrammes,[object] $ListeProgrammesD,[string] $nom)
 write-host $nom
 # cette fonction sert à vérifier l'existence d'un logiciel, et à récupérer son numéro de version
 if ('$ListeProgrammes' -match '$nom') {
@@ -27,12 +30,15 @@ write-host $nom " est absent"
 }
 $nom = "CCleaner"
 write-host $nom
-test_existence_logiciel($ListeProgrammes,$ListeProgrammesD,$nom)
-$nom = "filezilla"
+test_existence_logiciel $ListeProgrammes $ListeProgrammesD $nom
+$nom = "LibreOffice"
 write-host $nom
-test_existence_logiciel($ListeProgrammes,$ListeProgrammesD,$nom)
+test_existence_logiciel $ListeProgrammes $ListeProgrammesD $nom
+$nom = "Blender"
+write-host $nom
+test_existence_logiciel $ListeProgrammes $ListeProgrammesD $nom
 $matches
 $nom = "toto"
 write-host $nom
-test_existence_logiciel($ListeProgrammes,$ListeProgrammesD,$nom)
+test_existence_logiciel  $ListeProgrammes $ListeProgrammesD $nom
 $matches
